0eca17edfccaaadbe3363946015a9c076b15fcaa
MichalFijak <michalfijak1@hotmail.com>
2025-04-10 20:34:56 +0200

    register page, forms, crispy

--- exercise1/exercise1/settings.py
+++ exercise1/exercise1/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy Forms Settings
+CRISPY_TEMPLATE_PACK = "bootstrap5"
