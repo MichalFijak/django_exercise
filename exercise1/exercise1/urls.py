2e748afb160b70e87c50b048548c944bdccb37af
MichalFijak <michalfijak1@hotmail.com>
2025-04-15 20:04:18 +0200

    signal, profile updates

--- exercise1/exercise1/urls.py
+++ exercise1/exercise1/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as users_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,6 @@ urlpatterns = [
     path('profile/', users_views.profile, name='profile'),
     path('', include('blog.url')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
